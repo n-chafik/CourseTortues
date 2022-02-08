3493f8f0ccb04fb1637637f5ff7b806496afea21
Lucas Ruston <46353518+Skrohk@users.noreply.github.com>
2022-02-08 10:16:38 +0100

    Modification de l'algorithme de téléchargement des données

--- DataRetrieving.py
+++ DataRetrieving.py
@@ -5,24 +5,45 @@ import time
 from datetime import datetime
 
 stop = False
-n = 1
+n = 30
 url = "http://tortues.ecoquery.os.univ-lyon1.fr/race/"
-type_of_race = "large"
-while not stop:
-    time.sleep(3)
-    # get information from cluster
-    response = requests.get(url+type_of_race)
-    data = response.json()
 
-    # Serializing json
-    json_object = json.dumps(data, indent=4)
 
-    # Archiving information
-    file_name = "archive" + str(n) + ".json"
-    with open(type_of_race+"/"+file_name, "w") as outfile:
-        outfile.write(json_object)
-    print("Archiving in file ", n, " ", datetime.now())
+def retrieving_infos(n):
+    previous_data = {
+        "large": [],
+        "medium": [],
+        "small": [],
+        "tiny": [],
+    }
 
-    n = n + 1
-    if n == 150:
-        stop = True
+    for loop in range(1, n):
+        try:
+            for type_of_race in ['large', 'medium', 'small', 'tiny']:
+                # get information from cluster
+                response = requests.get(url + type_of_race)
+                data = response.json()
+
+                # Check if we have downloaded the same data than the previous tick
+                if previous_data[type_of_race] != data:
+                    # Serializing json
+                    json_object = json.dumps(data, indent=4)
+
+                    # Archiving information
+                    file_name = "archive" + str(loop) + ".json"
+                    with open("./raw_data/" + type_of_race + "/" + file_name, "w") as outfile:
+                        outfile.write(json_object)
+                        previous_data[type_of_race] = data
+                    print("Archiving in file ", loop, " ", datetime.now())
+                else:
+                    print("File already downloaded")
+                    break
+
+            time.sleep(2.5)
+        except Exception as e:
+            print("An error have occured : ", e)
+
+
+def group_data_by_id():
+    pass
+
